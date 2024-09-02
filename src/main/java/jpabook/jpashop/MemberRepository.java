441409ae030486a5777fe350a0431521dc86b5a6
nowcow <hhjnn92@icloud.com>
2024-09-02 18:08:50 +0900

    Entity 설계 완료, 연관관계 메소드 작성.

--- src/main/java/jpabook/jpashop/MemberRepository.java
+++ src/main/java/jpabook/jpashop/MemberRepository.java
@@ -2,6 +2,7 @@ package jpabook.jpashop;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jpabook.jpashop.domain.Member;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -12,7 +13,6 @@ public class MemberRepository {
 
     public Long save(Member member){
         em.persist(member);
-        em.flush();
         return member.getId();
     }
 
